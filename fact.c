427eda2358ba21e73075e8d1e03b506fa366d8a6
Bryan C <7554866+bryancandi@users.noreply.github.com>
2025-09-02 07:08:35 -0700

    Add files via upload

---
+++ fact.c
@@ -0,0 +1,38 @@
+// Get factorial using recursion
+
+#include <ctype.h>
+#include <stdio.h>
+
+int fact(int n);
+
+int main(void)
+{
+    int n;
+    do
+    {
+        printf("Number: ");
+        int scan = scanf("%i", &n);
+        if (scan == 0)
+        {
+            // Clear the input buffer with getchar() when the user enters invalid input
+            while (getchar() != '\n');
+        }
+    }
+    while (n <= 0);
+
+    printf("Factorial of %i is: %i\n", n, fact(n));
+}
+
+int fact(int n)
+{
+    // Base case
+    if (n == 1)
+    {
+        return 1;
+    }
+    // Recursive case
+    else
+    {
+        return n * fact(n - 1);
+    }
+}
