8fb243163ccf9b057da02358705a1bfa910e9fc4
Bryan C <7554866+bryancandi@users.noreply.github.com>
2025-10-03 19:15:06 -0700

    Add files via upload

---
+++ count.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+#define IN 1  // inside a word
+#define OUT 0 // outside a word
+
+int main(void)
+{
+    printf("Type one or more sentences followed by Enter, then press CTRL+D to count:\n");
+    int c, nl, nw, nc, state;
+
+    state = OUT;
+    nl = nw = nc = 0;
+    while ((c = getchar()) != EOF)
+    {
+        ++nc;
+        if (c == '\n')
+        {
+            ++nl;
+        }
+        if (c == ' ' || c == '\n' || c == '\t')
+        {
+            state = OUT;
+        }
+        else if (state == OUT)
+        {
+            state = IN;
+            ++nw;
+        }
+    }
+    printf("Lines: %d\nWords: %d\nChars: %d\n", nl, nw, nc);
+}
