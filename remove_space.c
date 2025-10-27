f7025bc707ccd67830c47a5aec017408cdc07e13
Bryan C <7554866+bryancandi@users.noreply.github.com>
2025-10-27 07:50:33 -0700

    Add remove_space.c

---
+++ remove_space.c
@@ -0,0 +1,43 @@
+#include <ctype.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define BUFFER 1024
+
+int main(void)
+{
+    char s[BUFFER];
+    int j = 0;
+
+    if (fgets(s, BUFFER, stdin) == NULL)
+    {
+        return 1;
+    }
+    else
+    {
+        // remove trailing newline from fgets()
+        s[strcspn(s, "\n")] = 0;
+    }
+
+    char *t = malloc(strlen(s) + 1); 
+    if (t == NULL) {
+        perror("malloc failed");
+        exit(1);
+    }
+
+    for (int i = 0, n = strlen(s); i < n; i++)
+    {
+        if (!isspace(s[i]))
+        {
+            t[j] = s[i];
+            j++;
+        }
+    }
+    t[j] = '\0';
+
+    printf("Original: %s\n", s);
+    printf("No Space: %s\n", t);
+
+    free(t);
+}
