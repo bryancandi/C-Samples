9736f982ee209595da5454aa7f87e9aff1228115
bryancandi <quakeaz@gmail.com>
2025-10-30 23:27:54 -0700

    Initial commit

---
+++ prefix_postfix.c
@@ -0,0 +1,32 @@
+/*
+ * Demonstration of post-increment/decrement (i++, i--) 
+ * versus pre-increment/decrement (++i, --i).
+ */
+
+
+#include <stdio.h>
+
+void print_num(int n);
+
+int main(void)
+{
+    int i;
+
+    printf("i = 1\n\n");
+
+    i = 1;
+    printf("i++:  ");
+    print_num(i++); // return current value of 'i', then increment
+
+    i = 1;
+    printf("++i:  ");
+    print_num(++i); // increment 'i', then return the new value
+
+
+    return 0;
+}
+
+void print_num(int n)
+{
+    printf("%d\n", n);
+}
