dcf7622cddb2dae03b6f3ec874908747f18605b0
bryancandi <quakeaz@gmail.com>
2025-10-30 23:30:57 -0700

    Initial commit

--- prefix_postfix.c
+++ prefix_postfix.c
@@ -3,7 +3,6 @@
  * versus pre-increment/decrement (++i, --i).
  */
 
-
 #include <stdio.h>
 
 void print_num(int n);
@@ -21,7 +20,6 @@ int main(void)
     i = 1;
     printf("++i:  ");
     print_num(++i); // increment 'i', then return the new value
-
 
     return 0;
 }
