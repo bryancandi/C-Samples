cf4f803012fad995d6e2512d34c1da61ae8bfd32
Bryan C <7554866+bryancandi@users.noreply.github.com>
2025-11-03 15:07:50 -0700

    Update comments for clarity

--- kr_book/squeeze.c
+++ kr_book/squeeze.c
@@ -21,7 +21,7 @@ int main(void)
 
 }
 
-// any: return location of first matching character from s2 in s1
+// any: scan s1 for characters in s2 that match; if match is found print location of match
 int any(char s1[], char s2[])
 {
     int i, j, k;
@@ -47,9 +47,12 @@ void squeeze(char s1[], char s2[])
 
     for (i = j = 0; s1[i] != '\0'; i++)
     {
+        // scan s2 to see if s1[i] matches any character
+        // loop stops early if a match is found; otherwise k reaches end of s2
         for (k = 0; s2[k] != '\0' && s1[i] != s2[k]; k++)
             ;
-
+        // if no match was found in s2 (s2[k] == '\0'), keep s1[i] by copying it to s1[j]
+        // advance j (j++) for next loop
         if (s2[k] == '\0')
         {
             s1[j++] = s1[i];
