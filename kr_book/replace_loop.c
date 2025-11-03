9fac62d84573166034d73029bfeb1f4fc70ca1eb
bryancandi <quakeaz@gmail.com>
2025-11-03 10:33:03 -0700

    Initial commit

---
+++ kr_book/replace_loop.c
@@ -0,0 +1,52 @@
+/* Exercise 2-2 replace for loop with equivalent */
+
+#include <stdio.h>
+#define MAXLINE 10
+
+int my_getline(char[], int maxline);
+
+// print input line
+int main(void)
+{
+    int len;    // current line length
+    char line[MAXLINE];     // current input line
+
+    while ((len = my_getline(line, MAXLINE)) > 0)
+    {
+        printf("%s", line);
+    }
+    return 0;
+}
+
+// my_getline: read a line into s, return length
+int my_getline(char s[], int lim)
+{
+    int c;
+    int i = 0;
+
+    while (i < lim - 1)
+    {
+        c = getchar();
+
+        if (c == EOF)
+        {
+            break; // stop on EOF
+        }
+
+        if (c == '\n')
+        {
+            break; // stop on newline
+        }
+
+        s[i] = c;
+        ++i;
+    }
+
+    if (c == '\n')
+    {
+        s[i] = c;
+        ++i;
+    }
+    s[i] = '\0';
+    return i;
+}
