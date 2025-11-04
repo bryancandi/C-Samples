c1d3a18694e78247c4de56a0aba12631aecc2ee7
Bryan C <7554866+bryancandi@users.noreply.github.com>
2025-11-03 17:01:42 -0700

    Initial commit

---
+++ kr_book/bitcount.c
@@ -0,0 +1,31 @@
+/* Section 2.10 expample - bitcount */
+
+#include <stdio.h>
+
+int bitcount(unsigned x);
+
+int main(void)
+{
+    unsigned x = 0b10110010;
+
+    int result = bitcount(x);
+
+    printf("%d\n", result); // returns 4 as 10110010 has 4 bits
+}
+
+// count '1' bits in x
+int bitcount(unsigned x)
+{
+    int b;
+    // Shift x right by 1, effectively examining each bit one by one from right to left.
+    for (b = 0; x != 0; x >>= 1)
+    {
+        // Check the least significant bit (LSB)
+        // If LSB is 1, increment b
+        if (x & 1)
+        {
+            b++;
+        }
+    }
+    return b;
+}
