68fe89ed6adc4ca994a81fbaed3cd7b09e396aff
Bryan C <7554866+bryancandi@users.noreply.github.com>
2025-11-04 10:04:49 -0700

    Initial commit

---
+++ kr_book/lower_1.c
@@ -0,0 +1,34 @@
+/* Sample 2.7
+ * lower: convert c to lower case (ASCII)
+ */
+
+#include <stdio.h>
+
+int lower(int c);
+
+int main(void)
+{
+    int c;
+    int lc;
+
+    while ((c = getchar()) != EOF)
+    {
+        lc = lower(c);
+        putchar(lc);
+    }
+
+}
+
+// converts an uppercase ASCII letter 'c' to lowercase using the formula c + 'a' - 'A'
+// otherwise return unmodified
+int lower(int c)
+{
+    if (c >= 'A' && c <= 'Z')
+    {
+        return c + 'a' - 'A';
+    }
+    else
+    {
+        return c;
+    }
+}
---
+++ kr_book/lower_2.c
@@ -0,0 +1,28 @@
+/* Exercise 2-10
+ * lower v2: convert c to lower case (ASCII) using a conditional expression
+ */
+
+#include <stdio.h>
+
+int lower(int c);
+
+int main(void)
+{
+    int c;
+    int lc;
+
+    while ((c = getchar()) != EOF)
+    {
+        lc = lower(c);
+        putchar(lc);
+    }
+
+}
+
+// use conditional expression to:
+// converts an uppercase ASCII letter 'c' to lowercase using the formula c + 'a' - 'A'
+// otherwise returne unmodified
+int lower(int c)
+{
+    return (c >= 'A' && c <= 'Z') ? (c + 'a' - 'A') : c;
+}
