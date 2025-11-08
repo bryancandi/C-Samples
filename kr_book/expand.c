c8c99b7a735be4d83ef7a9b64f295140aa4e3d05
Bryan C <7554866+bryancandi@users.noreply.github.com>
2025-11-07 19:53:11 -0700

    Initial commit

---
+++ kr_book/expand.c
@@ -0,0 +1,50 @@
+/* Exercise 3-3 - expand shorthand */
+
+#include <stdio.h>
+
+#define BUFFER 1000
+
+void expand(char s1[], char s2[]);
+
+int main(void)
+{
+    int c;
+    int i = 0;
+    char s[BUFFER];
+    char e[BUFFER];
+
+    while ((c = getchar()) != EOF && c != '\n' && i < BUFFER - 1)
+    {
+        s[i++] = c;
+    }
+    s[i] = '\0';
+
+    expand(s, e);
+    printf("%s\n", e);
+
+    return 0;
+}
+
+void expand(char s1[], char s2[])
+{
+    char c;
+    int i, j;
+    i = j = 0;
+
+    while ((c = s1[i++]) != '\0') // get char from s1 as char c
+    {
+        if (s1[i] == '-' && s1[i + 1] >= c)
+        {
+            i++;
+            while (c < s1[i]) // expand shorthand
+            {
+                s2[j++] = c++;
+            }
+        }
+        else
+        {
+            s2[j++] = c; // otherwise copy characters into s2
+        }
+    }
+    s2[j] = '\0';
+}
---
+++ kr_book/reverse.c
@@ -0,0 +1,40 @@
+/* Sample 3.5
+ * reverse - reverse string s in place
+ */
+
+#include <stdio.h>
+#include <string.h>
+
+#define BUFFER 1000
+
+void reverse(char s[]);
+
+int main(void)
+{
+    int c;
+    int i = 0;
+    char s[BUFFER];
+
+    while ((c = getchar()) != EOF && c != '\n' && i < BUFFER - 1)
+    {
+        s[i++] = c;
+    }
+    s[i] = '\0';
+
+    reverse(s);
+    printf("%s\n", s);
+
+    return 0;
+}
+
+void reverse(char s[])
+{
+    int c, i, j;
+
+    for (i = 0, j = strlen(s) - 1; i < j; i++, j--)
+    {
+        c = s[i];
+        s[i] = s[j];
+        s[j] = c;
+    }
+}
