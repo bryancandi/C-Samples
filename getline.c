49926adeffa988704a442e50230eff3a8b876d0d
Bryan C <7554866+bryancandi@users.noreply.github.com>
2025-11-10 09:28:55 -0700

    Initial commit

---
+++ getline.c
@@ -0,0 +1,38 @@
+/* Custom getline function 
+ * renamed to my_getline() to avoid conflict with POSIX getline()
+ */
+
+#include <stdio.h>
+#define BUFFER 1000
+
+int my_getline(char s[], int lim);
+
+int main(void)
+{
+    char line[BUFFER];
+
+    printf("Enter text:\n");
+    while (my_getline(line, BUFFER) > 0)
+    {
+        printf("%s", line);
+    }
+
+    return 0;
+}
+
+int my_getline(char s[], int lim)
+{
+    int c, i;
+
+    i = 0;
+    while (--lim > 0 && (c = getchar()) != EOF && c != '\n')
+    {
+        s[i++] = c;
+    }
+    if (c == '\n')
+    {
+        s[i++] = c;
+    }
+    s[i] = '\0';
+    return i;
+}
