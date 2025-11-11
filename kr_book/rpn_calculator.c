f3904a1477f09856d85028d3b6d774e6df58cefd
Bryan C <7554866+bryancandi@users.noreply.github.com>
2025-11-11 11:27:10 -0700

    Initial commit

---
+++ kr_book/rpn_calculator.c
@@ -0,0 +1,147 @@
+/* Sample 4.3
+ * Reverse Polish Notation calculator
+ */
+
+#include <ctype.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#define MAXOP 100   // max size of operand or operator
+#define MAXVAL 100  // max depth of value stack
+#define BUFSIZE 100 // buffer size for getch/ungetch
+#define NUMBER '0'  // signal that a number was found
+
+int sp = 0;         // next free stack position
+double val[MAXVAL]; // value stack
+
+char buf[BUFSIZE];  // buffer for ungetch
+int bufp = 0;       // next free position in buf
+
+int getop(char []);
+void push(double);
+double pop(void);
+int getch(void);
+void ungetch(int);
+
+// Reverse Polish Calculator
+int main(void)
+{
+    int type;
+    double op2;
+    char s[MAXOP];
+
+    while ((type = getop(s)) != EOF)
+    {
+        switch (type)
+        {
+        case NUMBER:
+            push(atof(s));
+            break;
+        case '+':
+            push(pop() + pop());
+            break;
+        case '*':
+            push(pop() * pop());
+            break;
+        case '-':
+            op2 = pop();
+            push(pop() - op2);
+            break;
+        case '/':
+            op2 = pop();
+            if (op2 != 0.0)
+            {
+                push(pop() / op2);
+            }
+            else
+            {
+                printf("error: zero divisor\n");
+            }
+            break;
+        case '\n':
+            printf("\t%.8g\n", pop());
+            break;
+        default:
+            printf("error: unknown command %s\n", s);
+            break;
+        }
+    }
+    return 0;
+}
+
+// push: push f onto value stack
+void push(double f)
+{
+    if (sp < MAXVAL)
+    {
+        val[sp++] = f;
+    }
+    else
+    {
+        printf("error: stack full, can't push %g\n", f);
+    }
+}
+
+// pop: pop and return top value from stack
+double pop(void)
+{
+    if (sp > 0)
+    {
+        return val[--sp];
+    }
+    else
+    {
+        printf("error: stack empty\n");
+        return 0.0;
+    }
+}
+
+// getop: get next operator or numeric operand
+int getop(char s[])
+{
+    int i, c;
+
+    while ((s[0] = c = getch()) == ' ' || c == '\t')
+        ;
+    s[1] = '\0';
+    if (!isdigit(c) && c != '.')
+    {
+        return c; // not a number
+    }
+    i = 0;
+    if (isdigit(c)) // collect integer part
+    {
+        while (isdigit(s[++i] = c = getch()))
+            ;
+    }
+    if (c == '.') // collect fraction part
+    {
+        while (isdigit(s[++i] = c = getch()))
+            ;
+    }
+    s[i] = '\0';
+    if (c != EOF)
+    {
+        ungetch(c);
+    }
+    return NUMBER;
+}
+
+// getch: get a (possibly pushed back) character
+int getch(void)
+{
+    return (bufp > 0) ? buf[--bufp] : getchar();
+}
+
+// ungetch: push character back on input
+void ungetch(int c)
+{
+    if (bufp >= BUFSIZE)
+    {
+        printf("ungetch: too many characters\n");
+    }
+    else
+    {
+        buf[bufp++] = c;
+    }
+}
