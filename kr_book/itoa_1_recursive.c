b9d7bbf0528203625caa791930cfb82cbe0a9788
Bryan C <7554866+bryancandi@users.noreply.github.com>
2025-11-14 08:44:45 -0700

    Initial commit

---
+++ kr_book/itoa_1_recursive.c
@@ -0,0 +1,40 @@
+/* Exercise 4-12
+ * itoa: convert n to characters in s using recursion
+ */
+
+#include <stdio.h>
+#include <string.h>
+
+#define BUFFER 1000
+
+void itoa(int n, char s[]);
+void reverse(char s[]);
+
+int main(void)
+{
+    int n = -123;
+    char s[BUFFER];
+
+    itoa(n, s);
+    printf("Int: %d\n", n);
+    printf("Str: %s\n", s); // prints n converted to string s
+
+    return 0;
+}
+
+void itoa(int n, char s[])
+{
+    static int i = 0; // Initialized once (static)
+
+    if (n < 0)
+    {
+        s[i++] = '-';
+        n = -n;
+    }
+    if (n / 10)
+    {
+        itoa(n / 10, s);
+    }
+    s[i++] = n % 10 + '0';
+    s[i] = '\0';
+}
