5313d939e92ef3b9ce2a92fdd0d7f6efdb555a59
Bryan C <7554866+bryancandi@users.noreply.github.com>
2025-11-16 20:12:31 -0700

    Explicitly return 0 from main

--- swap.c
+++ swap.c
@@ -1,4 +1,4 @@
-// Swaps two integers using pointers
+/* swap.c - swaps two integers using pointers */
 
 #include <stdio.h>
 
@@ -12,6 +12,8 @@ int main(void)
     printf("x is %i, y is %i\n", x, y);
     swap(&x, &y); // pass the address of x and y to the swap function
     printf("x is %i, y is %i\n", x, y);
+
+    return 0;
 }
 
 // dereference with *
