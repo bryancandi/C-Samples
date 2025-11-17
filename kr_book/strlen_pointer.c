a6100dd6063a1dd17b2d525821d89ee3e9346f42
Bryan C <7554866+bryancandi@users.noreply.github.com>
2025-11-17 12:26:17 -0700

    Initial commit

---
+++ kr_book/strlen_pointer.c
@@ -0,0 +1,38 @@
+/* Sample 5.3 */
+
+#include <stdio.h>
+
+#define MAXLINE 1000
+
+int my_strlen(char s[]);
+
+int main(void)
+{
+    int i;
+    char c;
+    char line[MAXLINE];
+
+    for (i = 0; (c = getchar()) != EOF && c != '\n'; ++i)
+    {
+        line[i] = c;
+    }
+    line[i] = '\0';
+
+    int len = my_strlen(line);
+    printf("%i\n", len);
+
+    return 0;
+}
+
+// my_strlen: return length of 's' excluding '\0'
+int my_strlen(char *s)
+{
+    int n;
+
+    for (n = 0; *s != '\0'; s++)
+    {
+        n++;
+    }
+
+    return n;
+}
