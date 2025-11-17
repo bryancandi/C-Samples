73038098e4c6f633ef4c8186c218d3772eb8bf2d
Bryan C <7554866+bryancandi@users.noreply.github.com>
2025-11-16 20:05:27 -0700

    Initial commit

---
+++ swap.c
@@ -0,0 +1,23 @@
+// Swaps two integers using pointers
+
+#include <stdio.h>
+
+void swap(int *a, int *b);
+
+int main(void)
+{
+    int x = 1;
+    int y = 2;
+
+    printf("x is %i, y is %i\n", x, y);
+    swap(&x, &y); // pass the address of x and y to the swap function
+    printf("x is %i, y is %i\n", x, y);
+}
+
+// dereference with *
+void swap(int *a, int *b)
+{
+    int tmp = *a;
+    *a = *b;
+    *b = tmp;
+}
