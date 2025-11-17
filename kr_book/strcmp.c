bcfb68ecdada6997e892b5b7aa766d198bce5d8c
Bryan C <7554866+bryancandi@users.noreply.github.com>
2025-11-17 16:02:29 -0700

    Initial commit

---
+++ kr_book/strcmp.c
@@ -0,0 +1,31 @@
+/* Sample 5.5
+ * strcmp: return <0 if s<t, 0 if s==t, >0 if s>t
+ */
+
+#include <stdio.h>
+
+int my_strcmp(char *s, char *t);
+
+int main(void)
+{
+    char *s = "This is also a string.";
+    char *t = "This is also a string.";
+
+    printf("%d\n", my_strcmp(s, t));
+
+    return 0;
+}
+
+int my_strcmp(char *s, char *t)
+{
+    int i;
+
+    for (i = 0; s[i] == t[i]; i++)
+    {
+        if (s[i] == '\0')
+        {
+            return 0;
+        }
+    }
+    return s[i] - t[i];
+}
---
+++ kr_book/strcmp_pointer.c
@@ -0,0 +1,29 @@
+/* Sample 5.5
+ * strcmp: return <0 if s<t, 0 if s==t, >0 if s>t; pointer version
+ */
+
+#include <stdio.h>
+
+int my_strcmp(char *s, char *t);
+
+int main(void)
+{
+    char *s = "This is also a string.";
+    char *t = "This is also a string.";
+
+    printf("%d\n", my_strcmp(s, t));
+
+    return 0;
+}
+
+int my_strcmp(char *s, char *t)
+{
+    for (; *s == *t; s++, t++)
+    {
+        if (*s == '\0')
+        {
+            return 0;
+        }
+    }
+    return *s - *t;
+}
