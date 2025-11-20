1537d31ba4b62eb1297fa9dd878e7bdd0003fadc
bryancandi <quakeaz@gmail.com>
2025-11-20 09:13:58 -0700

    Add function calls and get_input function

--- kr_book/date_converter_1.c
+++ kr_book/date_converter_1.c
@@ -6,7 +6,9 @@
 
 int day_of_year(int year, int month, int day);
 void month_day(int year, int yearday, int *pmonth, int *pday);
+void get_input(int *n);
 
+// daytable[0] = non-leap year, daytable[1] = leap year
 static char daytable[2][13] = {
     {0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31},
     {0, 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31}
@@ -14,7 +16,29 @@ static char daytable[2][13] = {
 
 int main(void)
 {
-    // TODO: Implement function calls
+    int year, month, day;
+    int yearday, out_month, out_day;
+
+    // day_of_year
+    printf("Enter year: ");
+    get_input(&year);
+    printf("Enter month: ");
+    get_input(&month);
+    printf("Enter day: ");
+    get_input(&day);
+
+    printf("Day of year: %d\n\n", day_of_year(year, month, day));
+
+    // month_day
+    printf("Enter year: ");
+    get_input(&year);
+    printf("Enter day of year: ");
+    get_input(&yearday);
+
+    month_day(year, yearday, &out_month, &out_day);
+    printf("Month: %d, day: %d\n", out_month, out_day);
+
+    return 0;
 }
 
 // day_of_year: set day of year from month and day
@@ -42,4 +66,9 @@ void month_day(int year, int yearday, int *pmonth, int *pday)
     }
     *pmonth = i;
     *pday = yearday;
+}
+
+void get_input(int *n)
+{
+    scanf("%d", n);
 }
